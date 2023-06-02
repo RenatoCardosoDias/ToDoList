c45676263811836dfe102d7ff6cf8d08ee261c8f
Renato Dias <renatocardosodias@icloud.com>
2023-06-02 16:08:09 -0300

    LogViewViewModel
    	importando FirebaseAuth
    	Criando um Publisher errorMessage
    
    	criando a função  de validação
    	iniciando o errorMessagem como vazio
    	limpando todos os caracteres em branco da caixa de texto email e password e verificando se estão em branco
    	Fazendo validação no campo de texto e-mail, verificando se possui arroba e um ponto

--- ToDoList/ViewModels/LoginViewViewModel.swift
+++ ToDoList/ViewModels/LoginViewViewModel.swift
@@ -5,15 +5,38 @@
 //  Created by Renato on 30/05/23.
 //
 
-import Foundation
+import SwiftUI
+import FirebaseAuth
 
-class LoginViewViewModel: ObservableObject {
+class LoginViewViewModel: ObservableObject  {
 	@Published var email = ""
 	@Published var password = ""
+	@Published var errorMessage = ""
 
 	init() {} //end  init
 
-	func login() {}
+	func login() {
+		guard validate() else{
+			return
+		}
 
-	func validate() {}
+		//Try log in
+		Auth.auth().signIn(withEmail: email, password: password)
+	}
+
+	private func validate() -> Bool {
+		errorMessage = ""
+		guard !email.trimmingCharacters(in: .whitespaces).isEmpty, !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+
+			errorMessage = "Please fill in all fields."
+			return false
+		}
+		//renato.dias@staki.com.br
+		guard email.contains("@") && email.contains(".") else {
+			errorMessage = "Please enter valid e-mail."
+			return false
+		}
+		print("Called")
+		return true
+	}
 } //end class LoginViewViewModel
